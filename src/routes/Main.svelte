0004a2c57ba26deb78b6a3be85019336c7cfdf0b
Paul S <dedsecdarknet@gmail.com>
2022-07-02 14:05:43 +0530

    scroll to load

--- src/routes/Main.svelte
+++ src/routes/Main.svelte
@@ -1,21 +1,28 @@
 <script>
   import { onMount } from "svelte";
 
-  let expenses = [
+  let toDate = new Date(),
+    expenses = [
     // {_id: "62bfe4c4e693fb447b6a5478", timestamp: 1656743106288, note: 'Morning Test', amount: -20},
     // {_id: "62bf4284383b9d464ee8446f", timestamp: 1656701571152, note: 'My Note', amount: -45},
     // {_id: "26fb4e4c6e39bf44b7a64587", timestamp: 1656625175126, note: 'Some Note', amount: -60},
     // {_id: "62b99bc11538b0aec75f047b", timestamp: 1656331200800, note: 'TXN-442', amount: 240},
-  ], gain, loss, toDate = new Date();
-
+  ],
+    gain,
+    loss,
+    more = false;
   toDate.setHours(0, 0, 0, 0);
 
-  $: gain = (expenses.filter(exp => exp.amount > 0).reduce((a,s) => a+s.amount, 0));
-  
-  $: loss = (expenses.filter(exp => exp.amount < 0).reduce((a,s) => a+s.amount, 0));
-  
-  
-  onMount(async () => {
+  $: gain = +expenses
+    .filter((exp) => exp.amount > 0)
+    .reduce((a, s) => a + s.amount, 0);
+  $: loss = -expenses
+    .filter((exp) => exp.amount < 0)
+    .reduce((a, s) => a + s.amount, 0);
+
+  const getExps = async () => {
+    console.log(toDate.getTime());
+    more = true;
     try {
       const res = await fetch(`/api?to=${toDate.getTime()}`);
       const { data, error } = await res.json();
@@ -25,8 +32,20 @@
       } else console.warn(res);
     } catch (err) {
       console.warn(err);
+    } finally {
+      more = false;
     }
-  });
+  };
+
+  onMount(getExps);
+
+  function handleScroll(event) {
+    if (!more && event.target.scrollTop + 10 > event.target.scrollHeight - event.target.clientHeight)
+      getExps();
+    console.log("scrollTop", event.target.scrollTop);
+    console.log("clientHeight", event.target.clientHeight);
+    console.log("scrollHeight", event.target.scrollHeight);
+  }
 </script>
 
 <header>
@@ -35,14 +54,13 @@
   <strong class="bl">${gain - loss}</strong>
   <strong class="re">${loss}</strong>
 </header>
-<section>
+<section on:scroll={handleScroll}>
   <!-- expense list -->
   {#each expenses as expense (expense._id)}
     <div class={expense.amount < 0 ? "re" : "gr"}>
-      <!-- 23-05-22 16:47 $24 -->
       {new Date(expense.timestamp).toLocaleDateString()}
       {new Date(expense.timestamp).toLocaleTimeString()}
-      ${expense.amount} - {expense.note} * {expense._id}
+      ${expense.amount} - {expense.note}
     </div>
   {/each}
 </section>
